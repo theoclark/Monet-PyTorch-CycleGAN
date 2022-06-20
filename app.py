9d1f0baeff2d56361847e1aeefe4ac8c1ed0eabd
theoclark <103810963+theoclark@users.noreply.github.com>
2022-06-20 10:37:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,7 @@ def upload_file():
     if uploaded_file.filename != '':
         os.mkdir('./static/Images')
         uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'original_image.jpg'))
-        # model = Model('./Model/G_xy.pt', './static/Images/input_image.jpg', './static/Images/output_image.jpg')
         model.predict("./static/Images/original_image.jpg" )
-        # del(model)
     return render_template('index.html', show_images=True)
 
 @app.route('/download')
